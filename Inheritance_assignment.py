3ec9038ff366e7df7b2d9b9d9bbb7ea0d805c789
Zsaigal <139230936+Zsaigal@users.noreply.github.com>
2024-01-20 15:03:25 +0500

    Add files via upload

---
+++ Inheritance_assignment.py
@@ -0,0 +1,16 @@
+
+
+#Class and inheritance
+
+class Animal:
+    name=''
+    age=''
+    breed=''
+
+class Dog(Animal):
+    Legs=4
+    Body_type='Has a tail'
+
+class Rabbit(Animal):
+    Color="White"
+    Price="$ 20"
